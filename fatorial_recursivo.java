52d7d8a060cf7297ee6bb34e55ae217a6046c72f
Cristiano Vieira <47859813+profcristianovs@users.noreply.github.com>
2019-12-03 21:34:34 -0300

    Create fatorial_recursivo.java

---
+++ fatorial_recursivo.java
@@ -0,0 +1,18 @@
+public class FatorialMain {
+	public static void main(String[] args) {
+		String numero = JOptionPane.showInputDialog("Calcular fatorial do numero:" );
+		int num = Integer.parseInt(numero);		
+		JOptionPane.showMessageDialog(null,"Fatorial de " + num + " é " + fatorialDoNumero(num));		
+	}
+	
+	private static long fatorialDoNumero(int num) {
+		int anterior = num-1;
+		long fatorialDesteNumero = 0;
+		
+		if (num <= 1) return 1;
+			else fatorialDesteNumero =  num * fatorialDoNumero(anterior); // Aqui está a recursividade no programa. O método "fatorialDoNumero" é chamado várias vezes dentro dele mesmo.
+		
+		return fatorialDesteNumero;
+	}
+
+}
