13f463b4f7731ff043387d8bb9b809e91bebe316
Cristiano Vieira <47859813+profcristianovs@users.noreply.github.com>
2019-12-03 21:13:11 -0300

    Update Procedimentos_Exercicio04.java

--- Procedimentos_Exercicio04.java
+++ Procedimentos_Exercicio04.java
@@ -3,8 +3,9 @@ public class primo {
 
 	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in); 
+		
 		System.out.print("Digite um numero: ");
-		Scanner scanner = new Scanner(System.in);    
 		int numero = scanner.nextInt();
 		
 		int resultado = verificar(numero);
